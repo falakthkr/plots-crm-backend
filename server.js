15c35160afd57f95f0e83a53edbb441d748ca5a4
Falak <falak-wareiq@Falaks-MacBook-Pro.local>
2024-01-26 21:47:06 +0530

    edited cors configs

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://plots-crm.netlify.app/", // specify the allowed origin
+    origin: "https://plots-crm.netlify.app", // specify the allowed origin
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // enable credentials (cookies, authorization headers, etc.)
   })
